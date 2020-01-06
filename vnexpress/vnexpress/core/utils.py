e6ca6f1b9cf555a93d0d5a3ee21407358a874d8d
Tuan Luu <tuan.luu@asnet.com.vn>
2020-01-06 09:29:26 +0700

    Update get urls

--- vnexpress/vnexpress/core/utils.py
+++ vnexpress/vnexpress/core/utils.py
@@ -1,5 +1,21 @@
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
+
+
 def convert_to_mp4(url):
-    return url.replace('d1', 'v').replace(',240p,360p,480p,,/', '').replace('/vne/master.m3u8', '.mp4').split('/')
+    dic = {
+        'd1': 'v',
+        ',240p,360p,480p,,/': '',
+        ',240p,,/': '',
+        ',360p,,/': '',
+        ',480p,,/': '',
+        '/vne/master.m3u8': '.mp4',
+        '/index-v1-a1.m3u8': '.mp4'
+    }
+
+    return replace_all(url, dic).split('/')
 
 
 def get_new_url(url):
@@ -7,7 +23,7 @@ def get_new_url(url):
 
     new_url = []
     for index, item in enumerate(formatted_video):
-        if item not in new_url:
+        if item == 'video' and item not in new_url or item != 'video':
             new_url.append(item)
 
     return '/'.join(new_url), formatted_video[-1]
--- vnexpress/vnexpress/spiders/vnexpress_spider.py
+++ vnexpress/vnexpress/spiders/vnexpress_spider.py
@@ -6,20 +6,36 @@ import scrapy
 import urllib.request
 from scrapy.utils.project import get_project_settings
 
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+
 # project
 from vnexpress.core import utils as services
 
 settings = get_project_settings()
 
 VIDEOS_PATH = os.path.join(settings.get('BOT_NAME'), 'mp4')
+URLS_PATH = os.path.join(settings.get('BOT_NAME'), 'urls')
 
 
 class VnExpressSpider(scrapy.Spider):
     name = "video_vnexpress"
-    start_urls = [
-        'https://vnexpress.net/infographics/ngoi-sao-lon-gap-1-400-lan-mat-troi-co-the-sap-phat-no-4033020.html',
-        'https://vnexpress.net/infographics/su-ra-doi-cua-nhung-ngoi-sao-dau-tien-trong-vu-tru-4033285.html'
-    ]
+
+    def start_requests(self):
+        urls = []
+        url_file = os.path.join(URLS_PATH, 'urls.txt')
+        with open(url_file) as fp:
+            line = fp.readline()
+            cnt = 1
+            while line:
+                print("Line {}: {}".format(cnt, line.strip()))
+                line = fp.readline()
+                cnt += 1
+                urls.append(line)
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         print('Start download')
@@ -36,3 +52,27 @@ class VnExpressSpider(scrapy.Spider):
         urllib.request.urlretrieve(str(new_url), fullfilename)
 
         print('Download {0} finish!!!'.format(file_name))
+
+
+class KhoaHocSpider(scrapy.Spider):
+    name = "khoahoc_vnexpress"
+    starts_urls = [
+        'https://vnexpress.net/khoa-hoc/thuong-thuc'
+    ]
+
+    def parse(self, response):
+        print('Start extract')
+        urls = response.css('h4.title_news a.icon_commend::attr(href)').getall()
+
+        url_file = os.path.join(URLS_PATH, 'urls.txt')
+        with open(url_file, 'a') as file:
+            for url in urls:
+                file.write(url + '\n')
+
+        next_page = response.css('a.next::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            print('Next_page: ', next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+        print('Finished extract')
